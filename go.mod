f3f0b1eb823d437ca46e24080f936a9e4b2305e6
Daniel Alkemic Czuba <alkemic7@gmail.com>
2019-03-12 17:12:38 +0100

    Remove testify

--- go.mod
+++ go.mod
@@ -2,11 +2,7 @@ module gokanban
 
 require (
 	github.com/Alkemic/go-route v0.0.0-20190105173841-9a78f75c55ad
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/jinzhu/gorm v1.9.2 // indirect
-	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
 	github.com/mattn/go-sqlite3 v1.10.0
 	github.com/pkg/errors v0.8.1
 	github.com/russross/blackfriday v1.5.2
-	github.com/stretchr/testify v1.3.0
 )
--- helper/helper_test.go
+++ helper/helper_test.go
@@ -1,159 +1,181 @@
 package helper
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestPrepareCheckboxes(t *testing.T) {
-	assert.Equal(
-		t, prepareCheckboxes("*[ ] Test", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test</label>",
-	)
-	assert.Equal(
-		t, prepareCheckboxes("* [ ] Test", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test</label>",
-	)
-	assert.Equal(
-		t, prepareCheckboxes("* [ ]Test", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" />Test</label>",
-	)
-	assert.Equal(
-		t, prepareCheckboxes("* [] Test", 13),
-		"* [] Test",
-	)
-
-	assert.Equal(
-		t, prepareCheckboxes("* [x] Test", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" checked=\"checked\" /> Test</label>",
-	)
-	assert.Equal(
-		t, prepareCheckboxes("*[x]Test", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" checked=\"checked\" />Test</label>",
-	)
-
-	assert.Equal(
-		t, prepareCheckboxes("* ~~[x]Spam~~\n* ~~[x] Ham~~", 13),
-		"* ~~<input type=\"checkbox\" disabled />Spam~~\n* ~~<input type=\"checkbox\" disabled /> Ham~~",
-	)
-
-	assert.Equal(
-		t, prepareCheckboxes("*[ ] Test*[x]Test", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test*[x]Test</label>",
-	)
-
-	assert.Equal(
-		t, prepareCheckboxes("*[ ] Foo bar\n*[x] Field 2", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Foo bar</label>\n"+
-			"* <label ng-click=\"CheckToggle(13, 2, $event)\"><input type=\"checkbox\" checked=\"checked\" /> Field 2</label>",
-		"It should render two checkboxes, second checked for task id 13",
-	)
-
-	assert.Equal(
-		t, prepareCheckboxes("* [ ] Test\n  * [ ] Sub-test\n      * [ ] Sub-sub-test", 13),
-		"* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test</label>\n"+
-			"  * <label ng-click=\"CheckToggle(13, 2, $event)\"><input type=\"checkbox\" /> Sub-test</label>\n"+
-			"      * <label ng-click=\"CheckToggle(13, 3, $event)\"><input type=\"checkbox\" /> Sub-sub-test</label>",
-		"Should return list with intendation",
-	)
-}
-
 func TestRenderMarkdown(t *testing.T) {
-	assert.Equal(
-		t, RenderMarkdown("*List*\n\n* Foo\n* Bar"),
-		"<p><em>List</em></p>\n\n"+
-			"<ul>\n"+
-			"<li>Foo<br /></li>\n"+
-			"<li>Bar<br /></li>\n"+
-			"</ul>\n",
-		"It should render markdown",
-	)
+	tests := []struct {
+		name     string
+		text     string
+		expected string
+	}{
+		{
+			name:     "should render markdown properly",
+			text:     "*List*\n\n* Foo\n* Bar",
+			expected: "<p><em>List</em></p>\n\n<ul>\n<li>Foo<br /></li>\n<li>Bar<br /></li>\n</ul>\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRendered := RenderMarkdown(tt.text); gotRendered != tt.expected {
+				t.Errorf("RenderMarkdown() = %v, want %v", gotRendered, tt.expected)
+			}
+		})
+	}
 }
 
 func TestToggleCheckbox(t *testing.T) {
-	assert.Equal(
-		t, ToggleCheckbox("*[ ] Foo bar\n*[x] Field 2", 1),
-		"*[X] Foo bar\n*[x] Field 2",
-	)
-	assert.Equal(
-		t, ToggleCheckbox("*[ ] Foo bar\n*[X] Field 2", 2),
-		"*[ ] Foo bar\n*[ ] Field 2",
-	)
-
-	assert.Equal(
-		t, ToggleCheckbox("*[S] Foo bar\n*[x] Field 2", 1),
-		"*[S] Foo bar\n*[ ] Field 2",
-		"It should ignore improperlly setted up marked checkbox",
-	)
-
-	assert.Equal(
-		t, ToggleCheckbox("*[ ] Foo bar\n*[x] Field 2", 3),
-		"*[ ] Foo bar\n*[x] Field 2",
-		"It should quietly ignore out of range request",
-	)
-
-	assert.Equal(
-		t, ToggleCheckbox("*[ ] Foo bar\n*[x] Field 2", 3),
-		"*[ ] Foo bar\n*[x] Field 2",
-		"It should quietly ignore out of range request",
-	)
+	type args struct {
+		t string
+		n int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected string
+	}{
+		{
+			name:     "should check first checkbox",
+			args:     args{t: "*[ ] Foo bar\n*[x] Field 2", n: 1},
+			expected: "*[X] Foo bar\n*[x] Field 2",
+		}, {
+			name:     "should uncheck second checkbox",
+			args:     args{t: "*[ ] Foo bar\n*[X] Field 2", n: 2},
+			expected: "*[ ] Foo bar\n*[ ] Field 2",
+		}, {
+			name:     "should uncheck second checkbox with small x",
+			args:     args{t: "*[ ] Foo bar\n*[x] Field 2", n: 2},
+			expected: "*[ ] Foo bar\n*[ ] Field 2",
+		}, {
+			name:     "should check first occurrence of square brackets",
+			args:     args{t: "*[ ] Foo bar\n*[x] Field 2", n: 1},
+			expected: "*[ ] [ ] Foo bar\n*[ ] Field 2",
+		}, {
+			name:     "should check first checkbox ignoring [S]",
+			args:     args{t: "*[S] Foo bar\n*[x] Field 2", n: 1},
+			expected: "*[S] Foo bar\n*[ ] Field 2",
+		}, {
+			name:     "should silently ignore out of range request",
+			args:     args{t: "*[ ] Foo bar\n*[x] Field 2", n: 3},
+			expected: "*[ ] Foo bar\n*[x] Field 2",
+		}, {
+			name:     "should silently ignore out of range request",
+			args:     args{t: "*[ ] Foo bar\n*[x] Field 2", n: 3},
+			expected: "*[ ] Foo bar\n*[x] Field 2",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToggleCheckbox(tt.args.t, tt.args.n); got != tt.expected {
+				t.Errorf("ToggleCheckbox() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
 }
 
-func TestToggleSingleCheckbox(t *testing.T) {
-	assert.Equal(
-		t, toggleSingleCheckbox("*[ ] Foo bar"),
-		"*[X] Foo bar",
-	)
-	assert.Equal(
-		t, toggleSingleCheckbox("*[X] Foo bar"),
-		"*[ ] Foo bar",
-	)
-	assert.Equal(
-		t, toggleSingleCheckbox("*[x] Foo bar"),
-		"*[ ] Foo bar",
-	)
-
-	assert.Equal(
-		t, toggleSingleCheckbox("* [x] Foo bar"),
-		"* [ ] Foo bar",
-	)
-	assert.Equal(
-		t, toggleSingleCheckbox("*  [x] Foo bar"),
-		"*  [ ] Foo bar",
-	)
-
-	assert.Equal(
-		t, toggleSingleCheckbox("*[ ] [ ] Foo bar"),
-		"*[X] [ ] Foo bar",
-		"It should only change first occurence",
-	)
+func Test_prepareCheckboxes(t *testing.T) {
+	type args struct {
+		t  string
+		id uint
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected string
+	}{
+		{
+			name:     "",
+			args:     args{t: "*[ ] Test", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test</label>",
+		}, {
+			name:     "",
+			args:     args{t: "* [ ] Test", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test</label>",
+		}, {
+			name:     "",
+			args:     args{t: "* [ ]Test", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" />Test</label>",
+		}, {
+			name:     "",
+			args:     args{t: "* [] Test", id: 13},
+			expected: "* [] Test",
+		}, {
+			name:     "",
+			args:     args{t: "* [x] Test", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" checked=\"checked\" /> Test</label>",
+		}, {
+			name:     "",
+			args:     args{t: "*[x]Test", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" checked=\"checked\" />Test</label>",
+		}, {
+			name:     "",
+			args:     args{t: "* ~~[x]Spam~~\n* ~~[x] Ham~~", id: 13},
+			expected: "* ~~<input type=\"checkbox\" disabled />Spam~~\n* ~~<input type=\"checkbox\" disabled /> Ham~~",
+		}, {
+			name:     "",
+			args:     args{t: "*[ ] Test*[x]Test", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test*[x]Test</label>",
+		}, {
+			name: "It should render two checkboxes, second checked for task id 13",
+			args: args{t: "*[ ] Foo bar\n*[x] Field 2", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Foo bar</label>\n" +
+				"* <label ng-click=\"CheckToggle(13, 2, $event)\"><input type=\"checkbox\" checked=\"checked\" /> Field 2</label>",
+		}, {
+			name: "Should return list with intendation",
+			args: args{t: "* [ ] Test\n  * [ ] Sub-test\n      * [ ] Sub-sub-test", id: 13},
+			expected: "* <label ng-click=\"CheckToggle(13, 1, $event)\"><input type=\"checkbox\" /> Test</label>\n" +
+				"  * <label ng-click=\"CheckToggle(13, 2, $event)\"><input type=\"checkbox\" /> Sub-test</label>\n" +
+				"      * <label ng-click=\"CheckToggle(13, 3, $event)\"><input type=\"checkbox\" /> Sub-sub-test</label>",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRend := prepareCheckboxes(tt.args.t, tt.args.id); gotRend != tt.expected {
+				t.Errorf("prepareCheckboxes() = %v, want %v", gotRend, tt.expected)
+			}
+		})
+	}
 }
 
-func TestCalculateTaskProgress(t *testing.T) {
-	assert.Equal(
-		t, calculateTaskProgress("*[S] Foo bar"),
-		map[string]int(nil),
-	)
-
-	assert.Equal(
-		t, calculateTaskProgress("*[ ] Foo bar"),
-		map[string]int{"Done": 0, "ToDo": 1},
-	)
-	assert.Equal(
-		t, calculateTaskProgress("*[X] Foo bar"),
-		map[string]int{"Done": 1, "ToDo": 0},
-	)
-	assert.Equal(
-		t, calculateTaskProgress("*[X] Ham\n*[ ] Foo\n  *[X] Foo tar\n  *[ ] Foo rar"),
-		map[string]int{"Done": 2, "ToDo": 2},
-	)
-	assert.Equal(
-		t, calculateTaskProgress("*[X] Ham\n*[ ] Foo\n  *[X] Foo tar\n  *[ ] Foo rar\n  * ~~[ ] Foo lol~~"),
-		map[string]int{"Done": 2, "ToDo": 2},
-	)
-	assert.Equal(
-		t, calculateTaskProgress("*[X] Ham*[ ]Test\n*[ ] Foo\n  *[X] Foo tar*[ ]Test2\n  *[ ] Foo rar"),
-		map[string]int{"Done": 2, "ToDo": 2},
-	)
+func Test_calculateTaskProgress(t *testing.T) {
+	tests := []struct {
+		name     string
+		text     string
+		expected map[string]int
+	}{
+		{
+			name:     "return nil when no checkboxes",
+			text:     "*[S] Foo bar",
+			expected: map[string]int(nil),
+		}, {
+			name:     "properly calculate when one unchecked",
+			text:     "*[ ] Foo bar",
+			expected: map[string]int{"Done": 0, "ToDo": 1},
+		}, {
+			name:     "properly calculate when one checked",
+			text:     "*[X] Foo bar",
+			expected: map[string]int{"Done": 1, "ToDo": 0},
+		}, {
+			name:     "properly calculate when two of four checked",
+			text:     "*[X] Ham\n*[ ] Foo\n  *[X] Foo tar\n  *[ ] Foo rar",
+			expected: map[string]int{"Done": 2, "ToDo": 2},
+		}, {
+			name:     "properly ignore when one striked task",
+			text:     "*[X] Ham\n*[ ] Foo\n  *[X] Foo tar\n  *[ ] Foo rar\n  * ~~[ ] Foo lol~~",
+			expected: map[string]int{"Done": 2, "ToDo": 2},
+		}, {
+			name:     "properly ignore wrongly wrote checkboxes markdown",
+			text:     "*[X] Ham*[ ]Test\n*[ ] Foo\n  *[X] Foo tar*[ ]Test2\n  *[ ] Foo rar",
+			expected: map[string]int{"Done": 2, "ToDo": 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calculateTaskProgress(tt.text); !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("calculateTaskProgress() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
 }
